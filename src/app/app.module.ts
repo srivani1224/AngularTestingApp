e463a23fe7bd0a3600157340e58407ab455c2028
srivanireddy <srivanireddy1432@gmail.com>
2021-05-07 19:32:41 +0530

    component architecture and relation is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationComponent,
-    UserListComponent
+    UserListComponent,
+    ProductsComponent,
+    FooterComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
