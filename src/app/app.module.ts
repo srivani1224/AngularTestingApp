5b2e31e341c1e497645fad84e2a23f1358508725
srivanireddy <srivanireddy1432@gmail.com>
2021-05-09 20:52:11 +0530

    routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { UserListComponent } from './user-list/user-list.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     UserListComponent,
     ProductsComponent,
     FooterComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
