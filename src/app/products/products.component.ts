002d965abba8925d54685be775f0e9cd5afabedb
srivanireddy <srivanireddy1432@gmail.com>
2021-05-09 18:00:58 +0530

    parent to child components interaction

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,41 +11,42 @@ export class ProductsComponent  {
 
   products:Product[]=[
     {
-    productTitle:"Technology-1",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
+    productTitle:"Parrot",
+    description:"Parrots, also known as psittacines, are birds of the roughly 398 species, found mostly in tropical and subtropical regions.",
+    productImage:"https://wallpapercave.com/wp/wp1861231.jpg"
     },
     {
-    productTitle:"Technology-2",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://apicms.thestar.com.my/uploads/images/2020/08/24/831125.jpg"
-    },
-    
-    {
-    productTitle:"Technology-3",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
+    productTitle:"Pigeon",
+    description:"Columbidae is a bird family consisting of pigeons and doves. It is the only family in the order Columbiformes.",
+    productImage:"https://www.teahub.io/photos/full/32-327086_pigeon-flower-nature-good-night.jpg"
     },
     
     {
-    productTitle:"Technology-4",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://itconsultingindubai.files.wordpress.com/2016/03/choosing-a-technology-solution.jpg"
+    productTitle:"Kingfisher",
+    description:"Kingfishers or Alcedinidae are a family of small to medium-sized, brightly colored birds in the order Coraciiformes.",
+    productImage:"https://images7.alphacoders.com/416/416332.jpg"
     },
     
     {
-    productTitle:"Technology-5",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+    productTitle:"Sparrow",
+    description:"Old World sparrows are a family of small passerine birds. They are also known as true sparrows.House sparrows are noisy and gregarious.",
+    productImage:"https://inhabitat.com/wp-content/blogs.dir/1/files/2014/12/Warbler.jpg"
     },
     
     {
-    productTitle:"Technology-6",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
-    productImage:"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5577975.jpg"
+    productTitle:"Cuckoo",
+    description:"The cuckoos are a family of birds, Cuculidae, the sole taxon in the order Cuculiformes.The cuckoo family includes the common or European cuckoo.",
+    productImage:"http://news.bbcimg.co.uk/media/images/48409000/jpg/_48409045_cuckoo_466.jpg"
+    },
+    
+    {
+    productTitle:"HummingBird",
+    description:"Hummingbirds are birds native to the Americas and constitute the biological family Trochilidae. There are about 360 species.",
+    productImage:"https://wallpapercave.com/wp/lVnKOji.jpg"
     }
     ];
   
+    
     productsSentByChild=[];
     productCount:number=0;
 
